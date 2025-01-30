cefa91ebde2469a004b2bb42504bc48a1f8d453c
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-01-30 09:15:26 -0500

    Restructure fetch metadata process

--- utils/metadata.py
+++ utils/metadata.py
@@ -1,13 +1,21 @@
-from utils import ROOT_DIR, load_json_type_safe
-from utils.logging import LoggedClass, log_once
-from utils.data_types import AssessedBiomarkerEntity
-from .api import API_HANDLER_MAP
-from typing import Optional
+from typing import Optional, Union, overload, Literal
 import logging
-import json
 from pathlib import Path
 from time import sleep
 import requests
+from requests import Response
+from enum import Enum
+from dotenv import load_dotenv
+
+from utils import ROOT_DIR, load_json_type_safe
+from utils.logging import LoggedClass, log_once
+from utils.data_types import AssessedBiomarkerEntity, Citation
+from .api import ENTITY_HANDLERS, CITATION_HANDLERS, LIBRARY_CALL
+
+
+class ApiCallType(Enum):
+    ENTITY_TYPE = 1
+    CITATION = 2
 
 
 class Metadata(LoggedClass):
@@ -16,6 +24,7 @@ class Metadata(LoggedClass):
         self, max_retries: int = 3, timeout: int = 5, sleep_time: int = 1
     ) -> None:
         super().__init__()
+        load_dotenv()
         self._mapping_file_path = ROOT_DIR / "mapping_data" / "namespace_map.json"
         self.debug(f"Loading namespace map from {self._mapping_file_path}")
         self.namespace_map = load_json_type_safe(self._mapping_file_path, "dict")
@@ -75,13 +84,42 @@ class Metadata(LoggedClass):
             return None
         return ROOT_DIR / "mapping_data" / cache_file_name
 
-    def entity_type_api_call(
-        self, resource: str, id: str, entity_type: Optional[str] = None
+    @overload
+    def fetch_metadata(
+        self,
+        fetch_flag: bool,
+        call_type: Literal[ApiCallType.ENTITY_TYPE],
+        resource: str,
+        id: str,
     ) -> tuple[int, Optional[AssessedBiomarkerEntity]]:
-        """Get the entity type recommended name and synonym data.
+        pass
+
+    @overload 
+    def fetch_metadata(
+        self,
+        fetch_flag: bool,
+        call_type: Literal[ApiCallType.CITATION],
+        resource: str,
+        id: str,
+    ) -> tuple[int, Optional[Citation]]:
+        pass
+
+    def fetch_metadata(
+        self,
+        fetch_flag: bool,
+        call_type: ApiCallType,
+        resource: str,
+        id: str,
+    ) -> tuple[int, Optional[Union[AssessedBiomarkerEntity, Citation]]]:
+        """Entry point for making an API call to fetch metadata.
 
         Parameters
         ----------
+        fetch_flag: bool
+            Determines whether to attempt an API call if the data isn't found locally
+            in the cache files.
+        call_type: ApiCallType
+            The type of metadata being fetched.
         resource: str
             The resource to make the API call to.
         id: str
@@ -91,107 +129,111 @@ class Metadata(LoggedClass):
 
         Returns
         -------
-        (int, AssessedBiomarkerEntity or None)
+        (int, AssessedBiomarkerEntity or Citation or None)
             An int indicating how many API calls were made, and the AssessedBiomarkerEntity
-            data or None if the process failed.
+            data, Citation data, or None if the process failed.
         """
-        # Check the resource exists in the namespace map
-        exists, resource_clean = self._check_resource_existence(resource)
-        if not exists:
-            self.debug(f"Resource {resource} does not exist in namespace map")
-            return 0, None
+        resource_clean = self._clean_string(string=resource, lower=True)
+        id = self._clean_string(string=id, lower=False)
 
         # Check that the API endpoint exists in the namespace map
-        endpoint = self.get_api_endpoint(resource_clean)
-        if not endpoint:
-            msg = (
-                f"No API endpoint availble for {resource} of type {entity_type}"
-                if entity_type
-                else f"No API endpoint availble for {resource}"
-            )
-            log_once(self.logger, msg, logging.WARNING)
+        base_endpoint = self.get_api_endpoint(resource_clean)
+        if not base_endpoint:
+            log_once(self.logger, f"No API endpoint availble for {resource}", logging.WARNING)
             return 0, None
 
-        # Begin API call handling
-        self.debug(f"Making API call for {resource} with ID {id} (type: {entity_type})")
-
-        # First check the cache and make sure an API call is absolutely necessary
+        # Check that a corresponding cache file path exists
         cache_path = self.get_cache_path(resource)
         if cache_path is None:
             self.error(f"No cache path found for {resource}")
             return 0, None
-
-        cache = load_json_type_safe(cache_path, "dict")
-        if id in cache:
-            self.debug(f"Found cached data for {id}")
-            cached_record = cache[id]
-            assessed_biomarker_entity = AssessedBiomarkerEntity(
-                recommended_name=cached_record["recommended_name"],
-                synonyms=cached_record["synonyms"],
-            )
-            return 0, assessed_biomarker_entity
-
-        # Check that the corresponding API call handler exists for this resource
-        handler = API_HANDLER_MAP.get(resource_clean)
-        if handler is None:
-            self.warning(f"Corresponding API handler for {resource} does not exist")
+        if not cache_path.exists():
+            self.error(f"Cache file at {cache_path} does not exist")
             return 0, None
 
-        # Attempt API calls
+        # Load the cache file
+        cache = load_json_type_safe(cache_path, "dict")
+        # Check if entry is already in our cache file
+        if id in cache:
+            self.debug(f"Found cached data for {id}")
+            found: Union[AssessedBiomarkerEntity, Citation]
+            cached_record = cache[id]
+            match call_type:
+                case ApiCallType.ENTITY_TYPE:
+                    found = AssessedBiomarkerEntity(
+                        recommended_name=cached_record["recommended_name"],
+                        synonyms=cached_record["synonyms"],
+                    )
+                case ApiCallType.CITATION:
+                    found = Citation(
+                        title=cached_record["title"], 
+                        journal=cached_record["journal"], 
+                        authors=cached_record["authors"], 
+                        date=cached_record["publication_date"], 
+                        reference=[], 
+                        evidence=[]
+                    )
+            return 0, found
+
+        if not fetch_flag:
+            return 0, None
+
+        handler_map = (ENTITY_HANDLERS if call_type == ApiCallType.ENTITY_TYPE else CITATION_HANDLERS)
+
+        # Check that the corresponding API call handler exists for this resource
+        if base_endpoint == LIBRARY_CALL:
+            lib_handler = handler_map.get("library", {}).get(resource_clean)
+            if not lib_handler:
+                self.warning(f"No library handler found for {resource}, call type: {call_type}")
+                return 0, None
+            return lib_handler(id, self._max_retries, self._timeout, self._sleep_time)
+        else:
+            api_handler = handler_map.get("api", {}).get(resource_clean)
+            if not api_handler:
+                self.warning(f"No API handler found for {resource}")
+                return 0, None
+            api_call_count, response = self._api_call_handling(resource=resource_clean, endpoint=base_endpoint.format(id))
+            if response is None:
+                return api_call_count, None
+            processed_data = api_handler(response)
+            return api_call_count, processed_data
+            
+    def _api_call_handling(
+        self, resource: str, endpoint: str
+    ) -> tuple[int, Optional[Response]]:
+
         attempt = 0
         while attempt < self._max_retries:
             try:
-                # Make request
-                response = requests.get(endpoint.format(id), timeout=self._timeout)
+                response = requests.get(endpoint, timeout=self._timeout)
                 if response.status_code != 200:
                     self.error(
-                        f"API call failed for {id}: {response.status_code} - {response.text}"
+                        f"API call failed for endpoint: {endpoint}\nStatus code: {response.status_code}\nContent: {response.text}"
                     )
                     return attempt + 1, None
 
-                self.info(f"Made successful API call for {resource}:{id}")
-
-                # Pass the return response to the resource specific API handler
-                processed_data = API_HANDLER_MAP[resource_clean](response)
-                if processed_data is None:
-                    log_once(
-                        self.logger,
-                        f"Unable to process data for {resource}:{id}",
-                        logging.WARNING,
-                    )
-                    return attempt + 1, None
-                else:
-                    self.debug(f"Adding entry for {resource}:{id} to {cache_path}")
-                    cache[id] = processed_data.to_dict()
-                    with open(cache_path, "w") as f:
-                        json.dump(cache, f, index=2)
-                    return attempt + 1, processed_data
-
-            except requests.Timeout as e:
-                self.warning(
-                    f"Request timeout on attempt {attempt + 1} for {resource}:{id}\n{e}"
+                self.info(
+                    f"Made successful API call to {resource}, endpoint: {endpoint}"
                 )
-                attempt += 1
-                self.debug(f"Sleeping for {self._sleep_time} seconds...")
-                sleep(self._sleep_time)
-            except requests.ConnectionError as e:
+
+                return attempt + 1, response
+
+            except (requests.Timeout, requests.ConnectionError) as e:
                 self.warning(
-                    f"Connection error on attempt {attempt + 1} for {resource}:{id}\n{e}"
+                    f"Request {type(e).__name__} on attempt {attempt + 1} for endpoint {endpoint} from resource {resource}\n{e}"
                 )
-                attempt += 1
-                self.debug(f"Sleeping for {self._sleep_time} seconds...")
-                sleep(self._sleep_time)
             except Exception as e:
                 self.exception(
-                    f"Unexpected error during API call for {resource}:{id}\n{e}"
+                    f"Unexpected error during API call (attempt {attempt + 1}/{self._max_retries}) for endpoint {endpoint} from resource {resource}\n{e}"
                 )
-                attempt += 1
-                self.debug(f"Sleeping for {self._sleep_time} seconds...")
-                sleep(self._sleep_time)
+
+            attempt += 1
+            self.debug(f"Sleeping for {self._sleep_time} seconds...")
+            sleep(self._sleep_time)
 
         log_once(
             self.logger,
-            f"Failed to reach API for {resource}:{id} after {self._max_retries} attempts",
+            f"Failed to reach API for {resource}, at endpoint {endpoint} after {self._max_retries} attempts",
             logging.ERROR,
         )
         return attempt + 1, None
@@ -204,5 +246,7 @@ class Metadata(LoggedClass):
             return False, resource_clean
         return True, resource_clean
 
-    def _clean_string(self, string: str) -> str:
-        return string.strip().lower()
+    def _clean_string(self, string: str, lower: bool = True) -> str:
+        if lower:
+            return string.strip().lower()
+        return string.strip()
