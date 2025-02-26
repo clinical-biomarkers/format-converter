7c038c7f29fe91130bfb72aaeb284edb8bf62d0f
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-02-26 12:18:46 -0500

    Loosen core components comparison during existing entry processing

--- utils/data_types/tsv_types.py
+++ utils/data_types/tsv_types.py
@@ -41,8 +41,6 @@ class TSVRow:
         """
         if (
             component.biomarker == self.biomarker
-            and component.assessed_biomarker_entity.recommended_name.lower()
-            == self.assessed_biomarker_entity.lower()
             and component.assessed_biomarker_entity_id.to_dict()
             == self.assessed_biomarker_entity_id
             and component.assessed_entity_type.lower()
