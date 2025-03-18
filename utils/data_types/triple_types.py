a493c9412dfe510105668bdfdc4f82e8c0660a5e
Sean Kim <33474168+seankim658@users.noreply.github.com>
2025-03-18 13:02:39 -0400

    Better type control

--- utils/data_types/triple_types.py
+++ utils/data_types/triple_types.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from enum import Enum
 
 
 @dataclass
@@ -12,6 +11,61 @@ class Triple:
         return f"<{self.subject}> <{self.predicate}> <{self.object}> ."
 
 
-class TripleCategory(Enum):
-    SUBJECT_OBJECTS = "subject_objects"
-    PREDICATES = "predicates"
+@dataclass
+class TripleSubjectObjects:
+
+    @classmethod
+    def name(cls) -> str:
+        return "subject_objects"
+
+    @classmethod
+    def id_key(cls) -> str:
+        return "biomarker_id"
+
+    @classmethod
+    def canonical_key(cls) -> str:
+        return "biomarker_canonical_id"
+
+    @classmethod
+    def role_key(cls) -> str:
+        return "best_biomarker_role"
+
+    @classmethod
+    def role_check(cls, role: str) -> bool:
+        return role in [
+            "risk",
+            "diagnostic",
+            "prognostic",
+            "monitoring",
+            "predictive",
+            "response",
+            "safety",
+        ]
+
+
+@dataclass
+class TriplePredicates:
+
+    @classmethod
+    def name(cls) -> str:
+        return "predicates"
+
+    @classmethod
+    def change_key(cls) -> str:
+        return "biomarker_change"
+
+    @classmethod
+    def role_key(cls) -> str:
+        return "best_biomarker_role"
+
+    @classmethod
+    def specimen_key(cls) -> str:
+        return "specimen_sampled_from"
+
+    @classmethod
+    def condition_key(cls) -> str:
+        return "condition_role_indicator"
+
+    @classmethod
+    def condition_role_check(cls, role: str) -> bool:
+        return role in ["diagnostic", "risk", "monitoring", "prognostic"]
